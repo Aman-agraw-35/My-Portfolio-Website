45663f2dad3151f1a1034327aae4824ae20dbbd5
Aman Agrawal <aman.agraw.35@gmail.com>
2023-10-27 22:50:52 +0530

    nji

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,21 +2,19 @@ import React, { useRef, useEffect } from 'react';
 import {particlesCursor} from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js"
 import * as THREE from 'three';
 function ThreeJSComponent() {
-  // Create a ref to hold a reference to the canvas element
+  
   const canvasRef = useRef(null);
 
-  // Function to initialize your Three.js scene
   const initThreeJS = () => {
     const canvas = canvasRef.current;
 
-    // Check if the canvas element exists
     if (!canvas) {
       console.error('Canvas element not found.');
       return;
     }
 
 
-    const renderer = new THREE.WebGLRenderer({ canvas, alpha: true }); // Set alpha to true for a transparent background
+    const renderer = new THREE.WebGLRenderer({ canvas, alpha: true }); 
 
     const pc = particlesCursor({
         el: document.getElementById('app'),
